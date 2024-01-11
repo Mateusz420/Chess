d3756ee77e0f14007e7e9d70550e0c5ec520cdcc
matsuchy <152329292+matsuchy@users.noreply.github.com>
2024-01-11 19:00:56 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 import pieces_class
 import pandas
-import contextlib
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -15,7 +14,7 @@ board_x = 0
 board_y = 735
 all_tiles = []
 tiles = []
-rows = []
+x_coords = []
 
 for i in range(9):
     for j in range(8):
@@ -31,10 +30,10 @@ for i in range(9):
 
 for i in range(len(all_tiles)):
     if i % 8 == 0 and i != 0:
-        tiles.append(rows)
-        rows = [all_tiles[i]]
+        tiles.append(x_coords)
+        x_coords = [all_tiles[i]]
     else:
-        rows.append(all_tiles[i])
+        x_coords.append(all_tiles[i])
 
 # test
 for i in tiles:
@@ -49,20 +48,254 @@ screen.blit(chess_board, (0, 0))
 pieces.pieces_draw(screen)
 pygame.display.update()
 
-
+global MOVE
+MOVE ="SELECT_PIECE"
 # current piece and coords
-global current_piece
-current_piece = ""
-# print(current_piece)
-global current_x_coord
-current_x_coord = ""
-# print(current_x_coord)
-global current_y_coord
-current_y_coord = ""
-# print(current_y_coord)
+# global selected_piece
+# selected_piece = ""
+# global selected_field
+# selected_field = ""
+# print(selected_piece)
+# global selected_x_coord
+# selected_x_coord = ""
+# print(selected_x_coord)
+# global selected_y_coord
+# selected_y_coord = ""
+# print(selected_y_coord)
 
 
 
+def piece_move(piece_x_coord, piece_y_coord, field_x_coord, field_y_coord):
+    global MOVE
+    
+    selected_piece = df.at[piece_x_coord, piece_y_coord]
+    print("selected_piece")
+    if (selected_piece == pieces_class.Pieces().white_pawn):
+        print("white_pawn")
+        white_pawn_move(piece_x_coord, piece_y_coord, field_x_coord, field_y_coord)
+        
+    elif (selected_piece == pieces_class.Pieces().black_pawn):
+        print("black_pawn")
+        black_pawn_move(piece_x_coord, piece_y_coord, field_x_coord, field_y_coord)
+        
+    elif (selected_piece == pieces_class.Pieces().white_knight):
+        print("white_knight")
+        white_knight_move(piece_x_coord, piece_y_coord, field_x_coord, field_y_coord)
+        
+    elif (selected_piece == pieces_class.Pieces().black_knight):
+        print("black_knight")
+        black_knight_move(piece_x_coord, piece_y_coord, field_x_coord, field_y_coord)
+        
+        
+        
+    elif (selected_piece == "0"):
+        print("0")
+        MOVE = "SELECT_PIECE"
+
+
+
+
+
+
+
+def white_pawn_move(piece_x_coord, piece_y_coord, field_x_coord, field_y_coord):
+    ### PION PO DOJŚCIU DO KOŃCA MOŻE PRZESKAKIWAĆ NA KOLEJNE KOLUMNY, BUG, KTÓRY PO DODANIU AWANSU PRZESTANIE DZIAŁAĆ
+    ### BRAK EN PASSANT
+
+    
+    
+    global MOVE
+    MOVE="SELECT_PIECE"
+    print("white_pawn_move")
+        
+    #bicie
+    if piece_x_coord - field_x_coord == 1 and df.at[field_x_coord, field_y_coord] != "0" and piece_y_coord - field_y_coord == 1 or piece_y_coord - field_y_coord == -1 and df.at[field_x_coord, field_y_coord] != "0":
+        print("white_pawn_bicie")
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_pawn
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print(df)
+        MOVE="SELECT_PIECE"
+        
+    #ruch o 2 pola
+    elif piece_y_coord == field_y_coord and piece_x_coord == 6 and field_x_coord == 4:
+        print("white_pawn_rucho2")
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_pawn
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print(df)
+        MOVE="SELECT_PIECE"
+        
+    #ruch o 1 pole
+    elif piece_y_coord == field_y_coord and piece_x_coord - field_x_coord == 1:
+        if(df.at[field_x_coord, field_y_coord]) == "0":
+            df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_pawn
+            df.at[piece_x_coord, piece_y_coord] = "0"
+            print("white_pawn_rucho1")
+            print(df)
+            MOVE="SELECT_PIECE"
+        else:
+            MOVE="SELECT_PIECE"
+            
+
+def black_pawn_move(piece_x_coord, piece_y_coord, field_x_coord, field_y_coord):
+    ### PION PO DOJŚCIU DO KOŃCA MOŻE PRZESKAKIWAĆ NA KOLEJNE KOLUMNY, BUG, KTÓRY PO DODANIU AWANSU PRZESTANIE DZIAŁAĆ
+    ### BRAK EN PASSANT
+    global MOVE
+    MOVE="SELECT_PIECE"
+    print("black_pawn_move")
+        
+    #bicie
+    if piece_x_coord + field_x_coord == 1 and df.at[field_x_coord, field_y_coord] != "0" and piece_y_coord - field_y_coord == 1 or piece_y_coord - field_y_coord == -1 and df.at[field_x_coord, field_y_coord] != "0":
+        print("black_pawn_bicie")
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_pawn
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print(df)
+        MOVE="SELECT_PIECE"
+        
+    #ruch o 2 pola
+    elif piece_y_coord == field_y_coord and piece_x_coord == 1 and field_x_coord == 3:
+        print("black_pawn_rucho2")
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_pawn
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print(df)
+        MOVE="SELECT_PIECE"
+        
+    #ruch o 1 pole
+    elif piece_y_coord == field_y_coord and piece_x_coord - field_x_coord == -1:
+        if(df.at[field_x_coord, field_y_coord]) == "0":
+            df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_pawn
+            df.at[piece_x_coord, piece_y_coord] = "0"
+            print("black_pawn_rucho1")
+            print(df)
+            MOVE="SELECT_PIECE"
+        else:
+            MOVE="SELECT_PIECE"
+
+
+
+def white_knight_move(piece_x_coord, piece_y_coord, field_x_coord, field_y_coord):
+    global MOVE
+    MOVE="SELECT_PIECE"
+    print("white_horse_move")
+    
+    #ruchy do przodu 2y+1x
+    if piece_x_coord - 2 == field_x_coord and piece_y_coord - 1 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("white_knight_rucho1")
+        print(df)
+        MOVE="SELECT_PIECE"
+    elif piece_x_coord - 2 == field_x_coord and piece_y_coord + 1 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("white_knight_rucho2")
+        print(df)
+        MOVE="SELECT_PIECE"
+        
+    #ruchy do tyłu -2y-1x
+    elif piece_x_coord + 2 == field_x_coord and piece_y_coord + 1 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("white_knight_rucho3")
+        print(df)
+        MOVE="SELECT_PIECE"
+    elif piece_x_coord + 2 == field_x_coord and piece_y_coord - 1 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("white_knight_rucho4")
+        print(df)
+        MOVE="SELECT_PIECE"
+        
+    #ruchy do przodu 1y+1x
+    elif piece_x_coord + 1 == field_x_coord and piece_y_coord + 2 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("white_knight_rucho3")
+        print(df)
+        MOVE="SELECT_PIECE"
+    elif piece_x_coord + 1 == field_x_coord and piece_y_coord - 2 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("white_knight_rucho4")
+        print(df)
+        MOVE="SELECT_PIECE"
+
+        #ruchy do tyłu -1y-1x
+    elif piece_x_coord - 1 == field_x_coord and piece_y_coord - 2 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("white_knight_rucho5")
+        print(df)
+        MOVE="SELECT_PIECE"
+    elif piece_x_coord - 1 == field_x_coord and piece_y_coord + 2 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().white_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("white_knight_rucho6")
+        print(df)
+        MOVE="SELECT_PIECE"
+
+    
+    
+    
+def black_knight_move(piece_x_coord, piece_y_coord, field_x_coord, field_y_coord):
+    global MOVE
+    MOVE="SELECT_PIECE"
+    print("black_horse_move")
+    
+    #ruchy do przodu 2y+1x
+    if piece_x_coord - 2 == field_x_coord and piece_y_coord - 1 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("black_knight_rucho1")
+        print(df)
+        MOVE="SELECT_PIECE"
+    elif piece_x_coord - 2 == field_x_coord and piece_y_coord + 1 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("black_knight_rucho2")
+        print(df)
+        MOVE="SELECT_PIECE"
+        
+    #ruchy do tyłu -2y-1x
+    elif piece_x_coord + 2 == field_x_coord and piece_y_coord + 1 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("black_knight_rucho3")
+        print(df)
+        MOVE="SELECT_PIECE"
+    elif piece_x_coord + 2 == field_x_coord and piece_y_coord - 1 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("black_knight_rucho4")
+        print(df)
+        MOVE="SELECT_PIECE"
+        
+    #ruchy do przodu 1y+1x
+    elif piece_x_coord + 1 == field_x_coord and piece_y_coord + 2 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("black_knight_rucho3")
+        print(df)
+        MOVE="SELECT_PIECE"
+    elif piece_x_coord + 1 == field_x_coord and piece_y_coord - 2 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("black_knight_rucho4")
+        print(df)
+        MOVE="SELECT_PIECE"
+
+        #ruchy do tyłu -1y-1x
+    elif piece_x_coord - 1 == field_x_coord and piece_y_coord - 2 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("black_knight_rucho5")
+        print(df)
+        MOVE="SELECT_PIECE"
+    elif piece_x_coord - 1 == field_x_coord and piece_y_coord + 2 == field_y_coord:
+        df.at[field_x_coord, field_y_coord] = pieces_class.Pieces().black_knight
+        df.at[piece_x_coord, piece_y_coord] = "0"
+        print("black_knight_rucho6")
+        print(df)
+        MOVE="SELECT_PIECE"
 
 
 
@@ -70,43 +303,9 @@ df = pandas.DataFrame(pieces_class.Pieces().starting_chess_board_data)
 print("wybierz figure")
 print(df)
 
-
-def piece_move(x_coord, y_coord):
-    
-    # global init
-    global current_piece
-    global current_x_coord
-    global current_y_coord
     
     
-    
-    
-    if(current_piece == ""):
-        current_piece = df.at[x_coord, y_coord]
-        current_x_coord = x_coord
-        current_y_coord = y_coord
-        # print(current_piece)
-        # print(current_x_coord)
-        # print(current_y_coord)
-        print("wybierz pole")
-        
-        
-        
-    elif(current_piece != ""):
-        
-        
-        df.at[x_coord, y_coord] = current_piece
-        
-        with contextlib.suppress(TypeError):
-            screen.blit((current_piece), (450, 450))
-            screen.blit((pieces_class.Pieces().blue_field), (200, 200))
-        
-        pygame.display.update()
-        current_piece = ""
-        df.at[current_x_coord, current_y_coord] = "0"
-        print("wybierz figure")
-        
-
+ 
 
 
 # events
@@ -118,14 +317,22 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            x_coord = event.pos[1] // 105
-            # print(x_coord)
-            y_coord = event.pos[0] // 105
-            # print(y_coord)
-            click_coords = [x_coord, y_coord]
-            print(click_coords)
+            if(MOVE == "SELECT_PIECE"):
+                piece_x_coord = event.pos[1] // 105
+                piece_y_coord = event.pos[0] // 105
+                piece_coords = [piece_x_coord, piece_y_coord]
+                print(f"piece cords {piece_coords}")
+                
+                MOVE = "SELECT_FIELD"
+                
+            elif(MOVE == "SELECT_FIELD"):
+                field_x_coord = event.pos[1] // 105
+                field_y_coord = event.pos[0] // 105
+                field_coords = [field_x_coord, field_y_coord]
+                print(f"field cords {field_coords}")
+                piece_move(piece_x_coord, piece_y_coord, field_x_coord, field_y_coord)
+                
+                
             
             
-            piece_move(x_coord, y_coord)
-            print(df)
             
--- pieces_class.py
+++ pieces_class.py
@@ -15,39 +15,37 @@ class Pieces:
     white_bishop = pygame.image.load("pieces\\white_bishop.png")
     white_king = pygame.image.load("pieces\\white_king.png")
     white_queen = pygame.image.load("pieces\\white_queen.png")
-    white_field = pygame.image.load("pieces\\white_field.png")
-    blue_field = pygame.image.load("pieces\\blue_field.png")
 
-    starting_chess_board_data = {0: [white_rook, white_pawn, "0", "0", "0", "0", black_pawn, black_rook],
-                                 1: [white_knight, white_pawn, "0", "0", "0", "0", black_pawn, black_knight],
-                                 2: [white_bishop, white_pawn, "0", "0", "0", "0", black_pawn, black_bishop],
-                                 3: [white_king, white_pawn, "0", "0", "0", "0", black_pawn, black_king],
-                                 4: [white_queen, white_pawn, "0", "0", "0", "0", black_pawn, black_queen],
-                                 5: [white_bishop, white_pawn, "0", "0", "0", "0", black_pawn, black_bishop],
-                                 6: [white_knight, white_pawn, "0", "0", "0", "0", black_pawn, black_knight],
-                                 7: [white_rook, white_pawn, "0", "0", "0", "0", black_pawn, black_rook]}
+    starting_chess_board_data = {"g": [black_rook, black_pawn, "0", "0", "0", "0", white_pawn, white_rook],
+                                 1: [black_knight, black_pawn, "0", "0", "0", "0", white_pawn, white_knight],
+                                 2: [black_bishop, black_pawn, "0", "0", "0", "0", white_pawn, white_bishop],
+                                 3: [black_king, black_pawn, "0", "0", "0", "0", white_pawn, white_king],
+                                 4: [black_queen, black_pawn, "0", "0", "0", "0", white_pawn, white_queen],
+                                 5: [black_bishop, black_pawn, "0", "0", "0", "0", white_pawn, white_bishop],
+                                 6: [black_knight, black_pawn, "0", "0", "0", "0", white_pawn, white_knight],
+                                 7: [black_rook, black_pawn, "0", "0", "0", "0", white_pawn, white_rook]}
 
     def pieces_draw(self, screen):
         for i in range(8):
-            screen.blit(self.black_pawn, ((0 + (i * 105)), 630))
-            screen.blit(self.white_pawn, ((0 + (i * 105)), 105))
+            screen.blit(self.white_pawn, ((0 + (i * 105)), 630))
+            screen.blit(self.black_pawn, ((0 + (i * 105)), 105))
 
         for i in range(2):
-            screen.blit(self.black_rook, ((0 + (i * (105 * 7))), 735))
-            screen.blit(self.white_rook, ((0 + (i * (105 * 7))), 0))
+            screen.blit(self.white_rook, ((0 + (i * (105 * 7))), 735))
+            screen.blit(self.black_rook, ((0 + (i * (105 * 7))), 0))
 
         for i in range(2):
-            screen.blit(self.black_knight, ((105 + (i * (105 * 5))), 735))
-            screen.blit(self.white_knight, ((105 + (i * (105 * 5))), 0))
+            screen.blit(self.white_knight, ((105 + (i * (105 * 5))), 735))
+            screen.blit(self.black_knight, ((105 + (i * (105 * 5))), 0))
 
         for i in range(2):
-            screen.blit(self.black_bishop, ((210 + (i * (105 * 3))), 735))
-            screen.blit(self.white_bishop, ((210 + (i * (105 * 3))), 0))
+            screen.blit(self.white_bishop, ((210 + (i * (105 * 3))), 735))
+            screen.blit(self.black_bishop, ((210 + (i * (105 * 3))), 0))
 
-        screen.blit(self.black_queen, (315, 735))
-        screen.blit(self.white_queen, (315, 0))
-        screen.blit(self.black_king, (420, 735))
-        screen.blit(self.white_king, (420, 0))
+        screen.blit(self.white_queen, (315, 735))
+        screen.blit(self.black_queen, (315, 0))
+        screen.blit(self.white_king, (420, 735))
+        screen.blit(self.black_king, (420, 0))
 
     # white = [[white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn],
     # [white_rook, white_knight, white_bishop, white_king, white_queen, white_bishop, white_knight, white_rook]]
